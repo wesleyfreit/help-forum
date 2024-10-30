7ff2edc7bd248fc805e146e3d7ace52557c4d4ba
Richard Wesley <wesleyrichard30@gmail.com>
2024-10-30 07:46:24 -0300

    feat: add use cases answer question, create question and get question by slug

---
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -0,0 +1,35 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository';
+import { Answer } from '@/domain/forum/enterprise/entities/answer';
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string;
+  questionId: string;
+  content: string;
+}
+
+interface AnswerQuestionUseCaseResponse {
+  answer: Answer;
+}
+
+export class AnswerQuestionUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
+    const answer = Answer.create({
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+      content,
+    });
+
+    await this.answersRepository.create(answer);
+
+    return {
+      answer,
+    };
+  }
+}
---
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -0,0 +1,29 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Question } from '@/domain/forum/enterprise/entities/question';
+import { QuestionsRepository } from '../repositories/questions-repository';
+
+interface GetQuestionBySlugUseCaseRequest {
+  slug: string;
+}
+
+interface GetQuestionBySlugUseCaseResponse {
+  question: Question;
+}
+
+export class GetQuestionBySlugUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    slug,
+  }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
+    const question = await this.questionsRepository.findBySlug(slug);
+
+    if (!question) {
+      throw new Error('Question not found');
+    }
+
+    return {
+      question,
+    };
+  }
+}
