5e0542d101fcd7dfedbe15433af0c01506ca16ba
Richard Freitas <wesleyrichard30@gmail.com>
2025-04-04 10:13:29 -0300

    feat: add fetch and delete question and answer comments use case, repositories methods and unit tests

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params';
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository';
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
 
@@ -6,5 +7,33 @@ export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepositor
 
   async create(answercomments: AnswerComment) {
     this.items.push(answercomments);
+  }
+
+  async findById(answerCommentId: string) {
+    const answerComment = this.items.find(
+      (answerComment) => answerComment.id.toString() === answerCommentId,
+    );
+
+    if (!answerComment) {
+      return null;
+    }
+
+    return answerComment;
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20);
+
+    return answerComments;
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const answerCommentIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    );
+
+    this.items.splice(answerCommentIndex, 1);
   }
 }
