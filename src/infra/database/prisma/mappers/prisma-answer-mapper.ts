e8ba141a44cab470b51bbf32b391fb8dcd8fa708
Richard Freitas <wesleyrichard30@gmail.com>
2025-04-18 12:36:44 -0300

    feat: add comment and attachment prisma models with relations, add all prisma to domain mappers

---
+++ src/infra/database/prisma/mappers/prisma-answer-mapper.ts
@@ -0,0 +1,29 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Answer } from '@/domain/forum/enterprise/entities/answer';
+import { Prisma, Answer as PrismaAnswer } from '@prisma/client';
+
+export class PrismaAnswerMapper {
+  static toDomain(raw: PrismaAnswer): Answer {
+    return Answer.create(
+      {
+        questionId: new UniqueEntityID(raw.questionId),
+        content: raw.content,
+        authorId: new UniqueEntityID(raw.authorId),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    );
+  }
+
+  static toPersistence(answer: Answer): Prisma.AnswerUncheckedCreateInput {
+    return {
+      questionId: answer.questionId.toString(),
+      id: answer.id.toString(),
+      content: answer.content,
+      authorId: answer.authorId.toString(),
+      createdAt: answer.createdAt,
+      updatedAt: answer.updatedAt,
+    };
+  }
+}
---
+++ src/infra/database/prisma/mappers/prisma-question-comment-mapper.ts
@@ -0,0 +1,35 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment';
+import { Prisma, Comment as PrismaComment } from '@prisma/client';
+
+export class PrismaQuestionCommentMapper {
+  static toDomain(raw: PrismaComment): QuestionComment {
+    if (!raw.questionId) {
+      throw new Error('Invalid comment type');
+    }
+
+    return QuestionComment.create(
+      {
+        questionId: new UniqueEntityID(raw.questionId),
+        content: raw.content,
+        authorId: new UniqueEntityID(raw.authorId),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityID(raw.id),
+    );
+  }
+
+  static toPersistence(
+    questionComment: QuestionComment,
+  ): Prisma.CommentUncheckedCreateInput {
+    return {
+      id: questionComment.id.toString(),
+      questionId: questionComment.questionId.toString(),
+      content: questionComment.content,
+      authorId: questionComment.authorId.toString(),
+      createdAt: questionComment.createdAt,
+      updatedAt: questionComment.updatedAt,
+    };
+  }
+}
